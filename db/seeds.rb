e421f46ea5da88261a020095beac607ceba89515
Marie-Laure Seguin <mlseguin@gmail.com>
2026-02-23 11:40:41 +0100

    creating app and article model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Article.create(title: 'The meaning of life', content: "It's 42!")
